9476400badb49eee2987ae1afd4e591e953bb739
James Rozos <12564338@student.uts.edu.au>
2017-08-28 20:04:25 +1000

    Password Reset v1

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ var passport = require('passport');
 var Account = require('../models/account');
 var router = express.Router();
 var Team = require('../models/team');
+
 var moment = require('moment');
+import * as ResetLinkController from "../controllers/ResetLinkController.js";
 import { ensureLoggedIn } from "connect-ensure-login"
 
 router.get('/', function (req, res) {
@@ -14,17 +16,34 @@ router.get('/register', function (req, res) {
     res.render('register', {});
 });
 
+router.get('/reset', function(req, res) {
+    res.render("reset", {rid: req.rid});
+});
+router.post('/reset', function(req, res) {
+    ResetLinkController.createLink(30,req.body.username)
+    res.render("reset", {snt: true, email: req.body.username});
+});
+
+router.get('/reset/confirm', function(req, res) {
+    //Get the user from the RID variable
+    //Load it into the view
+    ResetLinkController.resetFromRID(req,res);
+});
+router.post('/reset/confirmAction', function(req, res) {
+    //Put new password into the db
+    ResetLinkController.changePassword(req.body.RID,req.body.password,res)
+});
+
+
 router.post('/register', function (req, res, next) {
     Account.register(new Account({ username: req.body.username, firstName: req.body.firstName, lastName: req.body.lastName }), req.body.password, function (err, account) {
         if (err) {
             return res.render('register', { error: err.message });
         }
-
         passport.authenticate('local')(req, res, function () {
             req.session.save(function (err) {
                 if (err) {
                     console.log(err);
-
                 }
                 //     res.redirect('/');
             });
