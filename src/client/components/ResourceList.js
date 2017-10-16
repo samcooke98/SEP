3f772fbd80f7373a85f4b8f2c008a558165f695b
Sam Cooke <sam_cooke@outlook.com>
2017-10-16 20:23:02 +1100

    D-8 & D-7 (#22)
    * Made Feed view only show delete when user is admin
    
    * Resolved D-8
    Resolves D-7 as well.

--- src/client/components/ResourceList.js
+++ src/client/components/ResourceList.js
@@ -11,8 +11,9 @@ const ResourceList = (props) => {
     console.log(resources);
     return (
         <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: "wrap", flex: 1, flexDirection: 'row' }}>
-            { /*TODO: Show only teams that user belongs to, Sort the order */
-               resources.map((resource, i) => {
+            { /*TODO: Sort the order */
+                resources.map((resource, i) => {
+                    if(resource === null || resource === undefined) return null;
                     return <LinkCard
                         key={resource._id}
                         title={resource.title || ''}
@@ -21,14 +22,25 @@ const ResourceList = (props) => {
                         tags={resource.tags}
                         url={resource.url}
                         resourceId={resource._id}
-                        openFunc={() => console.log("TODO: OPEN")}
-                        commentFunc={() => console.log("TODO: COMMETNS")}
-                        removeFunc={() => console.log("TODO: DELETE")}
+                        openFunc={() => openURL(resource.url) }
+                        commentFunc={() => props.navigate(`/resource/${resource._id}/comments`)}
+                        removeFunc={() => props.deleteResource(resource._id)}
+                        showDelete={ props.userOwnedTeams.includes(resource.team)} 
                     />
                 })
             }
         </div>
     )
 }
+
+const openURL = (url) => {
+    window.open(
+        url.includes("http://") || url.includes('https://')
+            ? this.props.url
+            : "https://" + url
+    )
+}
+
+
 
 export default ResourceList;
