002d98f7b2afc26967e9e8669816ab5f811c7d24
Sam Cooke <sam@samcooke.com.au>
2017-10-17 11:48:39 +1100

    Resolves D-15: Email validation of inviting users

--- src/client/components/InviteDialog.js
+++ src/client/components/InviteDialog.js
@@ -4,7 +4,7 @@ Dialog to invite users to a team
 import React from "react";
 
 import { Dialog, Input, Button } from "react-toolbox";
-
+import isEmail  from "validator/lib/isEmail"
 export default class InviteDialog extends React.Component {
     constructor(props) {
         super(props);
@@ -23,16 +23,34 @@ export default class InviteDialog extends React.Component {
             >
                 <h2> Invite Users via Email </h2>
                 <p> Currently you can only add users via email. Please enter the email addresses below </p>
-                <Input 
-                    label="Emails: (john@smith.com; sally@peach.com)" 
-                    name='email' 
-                    error={this.props.error} 
-                    onChange={this.props.handleChange} 
-                    value={this.props.value} 
-                /> <br />
+                <form onSubmit={this.onSubmit}>
+                    <Input
+                        label="Emails: (john@smith.com; sally@peach.com)"
+                        name='email'
+                        error={this.state.error}
+                        onChange={this.props.handleChange}
+                        value={this.props.value}
+                    /> <br />
 
-                <Button name="sendInvite" icon="email" label="Send Invitations" onClick={this.props.onClick}/>
+                    <Button name="sendInvite" icon="email" label="Send Invitations" onClick={this.onSubmit} />
+                </form>
             </Dialog>
         )
+    }
+
+    onSubmit = ( evt ) => { 
+        evt.preventDefault();
+
+        let canSubmit = true;
+        const emails = this.props.value.split(';');
+        for(var email of emails) { 
+            console.log(email)
+            if (!isEmail(email)) {
+                canSubmit = false
+                this.setState({error: "Looks like one of the emails isn't valid"})
+            }
+        }
+        if(canSubmit) 
+            this.props.onClick();
     }
 }
