e3bfceadbc09e570e09102a75bdec1ab346ec8e2
Samuel Cooke <sam@samcooke.com.au>
2017-10-17 14:34:01 +1100

    D-24: Resolved - Passwords must match at PasswordReset

--- src/client/containers/PasswordResetContainer.js
+++ src/client/containers/PasswordResetContainer.js
@@ -15,11 +15,27 @@ class PasswordResetContainer extends React.Component {
         this.state = {
             success: null,
             msg: null,
+            error: '',
+            password: '',
+            passwordconfirm: ""
         }
+
+
     }
 
     handleChange = (name, value) => {
+        const state = { ...this.state, [name]: value };
         this.setState({ [name]: value })
+        this.calcErrors(state);
+    }
+
+    calcErrors = (state) => {
+        console.log(state)
+        if (state.password != state.passwordconfirm)
+            this.setState({ error: 'Passwords do not match' })
+        else {
+            this.setState({ error: '' })
+        }
     }
 
     submitForm = (evt) => {
@@ -28,6 +44,7 @@ class PasswordResetContainer extends React.Component {
         console.log(params.get('RID'))
         var action = resetPassConfirm(this.state.password, this.props.match.params.id || params.get('RID'))
         action.payload.then((val) => {
+            console.log(val)
             this.setState({ success: val.success, msg: val.payload })
         })
     }
@@ -45,12 +62,14 @@ class PasswordResetContainer extends React.Component {
             <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
                 <h1> Reset Password </h1>
                 <p> Enter your new password below </p>
-                {this.state.success != null && <p> Error: {this.state.msg}</p> }
-                {this.state.success && <p> Your password has been reset </p> }
-                <Input type='password' label='Password' name='password' value={this.state.password} onChange={this.handleChange.bind(this, "password")} />
-                <Input type='password' label="Confirm Password" name='passwordconfirm' value={this.state.passwordconfirm} onChange={this.handleChange.bind(this, "passwordconfirm")} />
-                <Button label='Submit' raised primary onClick={this.submitForm} />
-
+                {this.state.success === false && <p> Error: {this.state.msg}</p>}
+                {this.state.success && <p> Your password has been reset </p>}
+                <form onSubmit={this.submitForm}>
+                    <Input type='password' label='Password' name='password' value={this.state.password} error={this.state.error} onChange={this.handleChange.bind(this, "password")} />
+                    <Input type='password' label="Confirm Password" name='passwordconfirm' error={this.state.error} value={this.state.passwordconfirm} onChange={this.handleChange.bind(this, "passwordconfirm")} />
+                    <Button label='Submit' raised primary onClick={this.submitForm} />
+                    <input type='submit' style={{display: 'none'}} onSubmit={this.submitForm}/>
+                </form>
             </div>
         )
     }
